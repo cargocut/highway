64683f3eb78e0da6df93feed52895d721549a553
xvw <xavier.vdw@gmail.com>
2026-08-10 12:23:10 +0200

    ~`Lunar`~ -> `Highway`

--- lib/path.ml
+++ lib/path.ml
@@ -1,4 +1,4 @@
-(* Copyright (c) 2026, Cargocut and the Lunar developers.
+(* Copyright (c) 2026, Cargocut and the Highway developers.
    All rights reserved.
 
    SPDX-License-Identifier: BSD-3-Clause *)
