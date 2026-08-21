03551009db0b24af5eebd09c66791e071e48fc87
xvw <xavier.vdw@gmail.com>
2026-08-21 19:35:22 +0200

    Add `Highway-dream` library

---
+++ lib/dream/highway_dream.ml
@@ -0,0 +1,57 @@
+(* Copyright (c) 2026, Cargocut and the Highway developers.
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause *)
+
+type request = Dream.request
+type response = Dream.response Dream.promise
+type handler = (request, response) Highway.handler
+type middleware = (request, response) Highway.middleware
+type 'a context = ('a, request, response) Highway.context
+type service = (request, response) Highway.service
+
+let adapt_method : Dream.method_ -> Highway.meth option = function
+  | `Method s ->
+    (match String.(trim @@ lowercase_ascii s) with
+     | "query" -> Some `QUERY
+     | _ -> None)
+  | `GET -> Some `GET
+  | `POST -> Some `POST
+  | `PUT -> Some `PUT
+  | `DELETE -> Some `DELETE
+  | `HEAD -> Some `HEAD
+  | `CONNECT -> Some `CONNECT
+  | `OPTIONS -> Some `OPTIONS
+  | `TRACE -> Some `TRACE
+  | `PATCH -> Some `PATCH
+;;
+
+let adapt_target target =
+  match String.split_on_char '/' target with
+  | "" :: xs | xs -> xs
+;;
+
+let dispatch services fallback request =
+  match adapt_method (Dream.method_ request) with
+  | None -> fallback request
+  | Some given_method ->
+    let given_path = adapt_target (Dream.target request)
+    and given_query_params = Dream.all_queries request in
+    Highway.Service.dispatch
+      ~given_method
+      ~given_path
+      ~given_query_params
+      services
+      fallback
+      request
+;;
+
+let redirect ?status ?code ?headers ?anchor ?extra_params route args param req =
+  let target = Highway.html_href ?anchor ?extra_params route args param in
+  Dream.redirect ?status ?code ?headers req target
+;;
+
+let redirect' ?status ?code ?headers ?anchor route args param req =
+  let target = Highway.html_href' ?anchor route args param in
+  Dream.redirect ?status ?code ?headers req target
+;;
---
+++ lib/dream/highway_dream.mli
@@ -0,0 +1,63 @@
+(* Copyright (c) 2026, Cargocut and the Highway developers.
+   All rights reserved.
+
+   SPDX-License-Identifier: BSD-3-Clause *)
+
+(** Makes it easy to use Highway with
+    {{:https://camlworks.github.io/dream/} Dream}. *)
+
+(** {1 Types}
+
+    A few type aliases to simplify the following signatures. *)
+
+type request = Dream.request
+type response = Dream.response Dream.promise
+type handler = (request, response) Highway.handler
+type middleware = (request, response) Highway.middleware
+type 'a context = ('a, request, response) Highway.context
+type service = (request, response) Highway.service
+
+(** {1 Routing using Highway} *)
+
+(** [dispatch services] describes a middleware that attempts to route
+    one of the services in the [services] list (or moves on to the
+    next handler). *)
+val dispatch : service list -> middleware
+
+(** {1 Helpers} *)
+
+(** [redirect ?status ?code ?headers ?anchor ?extra_params route args params]
+    is an {!type:handler} that performs a redirection. *)
+val redirect
+  :  ?status:Dream.redirection
+  -> ?code:int
+  -> ?headers:(string * string) list
+  -> ?anchor:string
+  -> ?extra_params:(string * string) list
+  -> ( 'scope
+       , Highway.Method.for_html_links
+       , Highway.something
+       , 'param
+       , 'args )
+       Highway.route
+  -> 'args Highway.args
+  -> 'param
+  -> handler
+
+(** [redirect ?status ?code ?headers ?anchor route args params] is an
+    {!type:handler} that performs a redirection (without extra
+    params). *)
+val redirect'
+  :  ?status:Dream.redirection
+  -> ?code:int
+  -> ?headers:(string * string) list
+  -> ?anchor:string
+  -> ( 'scope
+       , Highway.Method.for_html_links
+       , 'cstrs
+       , 'param
+       , 'args )
+       Highway.route
+  -> 'args Highway.args
+  -> 'param
+  -> handler
